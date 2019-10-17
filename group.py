a8106b47255034490db754c08430b18e7e9fb44a
Lenny Aarons-Ditson <lad@eduroam-int-dhcp-97-238-36.ucl.ac.uk>
2019-10-17 11:48:24 +0100

    add group data structure with example instance

--- group.py
+++ group.py
@@ -3,3 +3,31 @@
 # Your code to go here...
 
 my_group =
+    {
+        {
+            'name': 'Jill',
+            'age': 26,
+            'job': 'biologist',
+            'friends' : {'Zalika'},
+            'partners' : {'John'}
+        },
+        {
+            'name': 'Zalika',
+            'age': 28,
+            'job': 'artist',
+            'friends' : {'Jill'}
+        },
+        {
+            'name': 'John',
+            'age': 27,
+            'job': 'writer',
+            'partners' : {'Jill'}
+        },
+        {
+            'name': 'Nash',
+            'age': 34,
+            'job': 'chef',
+            'cousins' : {'John'}
+            'tennants' : {'Jill'}
+        }
+    }
